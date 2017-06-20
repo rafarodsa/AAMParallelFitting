2cc5e437959d810761ed2e25c772532822b860ae
Rafael Rodriguez <rafaelrs307@gmail.com>
2017-06-21 01:08:01 +0200

    added modified aamlibrary

---
+++ aamlibrary/src/CMakeFiles/aamlibrary.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/rafaelrodriguez/Documents/Polimi/2Semestre/Advanced Algorithms and Parallel Programming/AAM_OpenMP/aamlibrary/src/AAM_Basic.cpp" "/Users/rafaelrodriguez/Documents/Polimi/2Semestre/Advanced Algorithms and Parallel Programming/AAM_OpenMP/aamlibrary/src/CMakeFiles/aamlibrary.dir/AAM_Basic.cpp.o"
+  "/Users/rafaelrodriguez/Documents/Polimi/2Semestre/Advanced Algorithms and Parallel Programming/AAM_OpenMP/aamlibrary/src/AAM_CAM.cpp" "/Users/rafaelrodriguez/Documents/Polimi/2Semestre/Advanced Algorithms and Parallel Programming/AAM_OpenMP/aamlibrary/src/CMakeFiles/aamlibrary.dir/AAM_CAM.cpp.o"
+  "/Users/rafaelrodriguez/Documents/Polimi/2Semestre/Advanced Algorithms and Parallel Programming/AAM_OpenMP/aamlibrary/src/AAM_IC.cpp" "/Users/rafaelrodriguez/Documents/Polimi/2Semestre/Advanced Algorithms and Parallel Programming/AAM_OpenMP/aamlibrary/src/CMakeFiles/aamlibrary.dir/AAM_IC.cpp.o"
+  "/Users/rafaelrodriguez/Documents/Polimi/2Semestre/Advanced Algorithms and Parallel Programming/AAM_OpenMP/aamlibrary/src/AAM_MovieAVI.cpp" "/Users/rafaelrodriguez/Documents/Polimi/2Semestre/Advanced Algorithms and Parallel Programming/AAM_OpenMP/aamlibrary/src/CMakeFiles/aamlibrary.dir/AAM_MovieAVI.cpp.o"
+  "/Users/rafaelrodriguez/Documents/Polimi/2Semestre/Advanced Algorithms and Parallel Programming/AAM_OpenMP/aamlibrary/src/AAM_PAW.cpp" "/Users/rafaelrodriguez/Documents/Polimi/2Semestre/Advanced Algorithms and Parallel Programming/AAM_OpenMP/aamlibrary/src/CMakeFiles/aamlibrary.dir/AAM_PAW.cpp.o"
+  "/Users/rafaelrodriguez/Documents/Polimi/2Semestre/Advanced Algorithms and Parallel Programming/AAM_OpenMP/aamlibrary/src/AAM_PDM.cpp" "/Users/rafaelrodriguez/Documents/Polimi/2Semestre/Advanced Algorithms and Parallel Programming/AAM_OpenMP/aamlibrary/src/CMakeFiles/aamlibrary.dir/AAM_PDM.cpp.o"
+  "/Users/rafaelrodriguez/Documents/Polimi/2Semestre/Advanced Algorithms and Parallel Programming/AAM_OpenMP/aamlibrary/src/AAM_Shape.cpp" "/Users/rafaelrodriguez/Documents/Polimi/2Semestre/Advanced Algorithms and Parallel Programming/AAM_OpenMP/aamlibrary/src/CMakeFiles/aamlibrary.dir/AAM_Shape.cpp.o"
+  "/Users/rafaelrodriguez/Documents/Polimi/2Semestre/Advanced Algorithms and Parallel Programming/AAM_OpenMP/aamlibrary/src/AAM_TDM.cpp" "/Users/rafaelrodriguez/Documents/Polimi/2Semestre/Advanced Algorithms and Parallel Programming/AAM_OpenMP/aamlibrary/src/CMakeFiles/aamlibrary.dir/AAM_TDM.cpp.o"
+  "/Users/rafaelrodriguez/Documents/Polimi/2Semestre/Advanced Algorithms and Parallel Programming/AAM_OpenMP/aamlibrary/src/AAM_Util.cpp" "/Users/rafaelrodriguez/Documents/Polimi/2Semestre/Advanced Algorithms and Parallel Programming/AAM_OpenMP/aamlibrary/src/CMakeFiles/aamlibrary.dir/AAM_Util.cpp.o"
+  "/Users/rafaelrodriguez/Documents/Polimi/2Semestre/Advanced Algorithms and Parallel Programming/AAM_OpenMP/aamlibrary/src/VJfacedetect.cpp" "/Users/rafaelrodriguez/Documents/Polimi/2Semestre/Advanced Algorithms and Parallel Programming/AAM_OpenMP/aamlibrary/src/CMakeFiles/aamlibrary.dir/VJfacedetect.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv"
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ aamlibrary/src/CMakeFiles/aamlibrary.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/aamlibrary.dir/AAM_PDM.cpp.o"
+  "CMakeFiles/aamlibrary.dir/AAM_TDM.cpp.o"
+  "CMakeFiles/aamlibrary.dir/AAM_Util.cpp.o"
+  "CMakeFiles/aamlibrary.dir/AAM_Shape.cpp.o"
+  "CMakeFiles/aamlibrary.dir/AAM_CAM.cpp.o"
+  "CMakeFiles/aamlibrary.dir/VJfacedetect.cpp.o"
+  "CMakeFiles/aamlibrary.dir/AAM_MovieAVI.cpp.o"
+  "CMakeFiles/aamlibrary.dir/AAM_PAW.cpp.o"
+  "CMakeFiles/aamlibrary.dir/AAM_Basic.cpp.o"
+  "CMakeFiles/aamlibrary.dir/AAM_IC.cpp.o"
+  "../../bin/libaamlibrary.pdb"
+  "../../bin/libaamlibrary.dylib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/aamlibrary.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
